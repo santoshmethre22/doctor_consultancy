364d549fd8d1db4ba7e7b592b7a114eeb45fdf98
santosh methre <santoshmethre22@gmail.com>
2025-05-17 20:26:06 +0530

    the upload method updated

--- server/src/controllers/upload.controllers.js
+++ server/src/controllers/upload.controllers.js
@@ -10,13 +10,40 @@ const uploadProfile=async()=>{
             return res.status(404).json({ message: "User not found" });
         }
 
-        
-    
+         const avatarLocalPath = req.files?.avatar[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+         const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    
+   // const user = await User.create({
+    //     fullName,
+    //     avatar: avatar.url,
+    //     coverImage: coverImage?.url || "",
+    //     email, 
+    //     password,
+    //     username: username.toLowerCase()
+    // })
 
     } catch (error) {
         
 
 
     }
+}
+
+
+// file upload for the patient aswell as the doctor--------------
+
+
+const uploadFile=async(req,res)=>{
+
+
+
 }
