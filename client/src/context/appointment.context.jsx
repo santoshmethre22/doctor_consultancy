e731dac97901c2d72e3f34b6787b981b84ac5463
santosh methre <santoshmethre22@gmail.com>
2025-05-23 22:19:47 +0530

    appointment method is implemened

--- client/src/context/appointment.context.jsx
+++ client/src/context/appointment.context.jsx
@@ -9,6 +9,7 @@ const api = axios.create({
   withCredentials: true,
 });
 
+// ----------------------------------------------------------------------------------------
 export const AppointmentProvider = ({ children }) => {
 
 
